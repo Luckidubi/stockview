7e5d9b56fcdade3e71dc82a9596286aae5f9a377
Dubem <nwodochidubem1@gmail.com>
2023-03-29 07:33:52 -0400

    chore: Update Autocomplete component

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import finnHub from '../apis/finnHub'
 
 function AutoComplete() {
+    const [search, setSearch] = useState()
+    const [results, setResults] = useState([])
+
+    const renderDropdown = () => {
+        const dropDownClass = search ? "show" : ""
+        return (
+        <ul style={{
+            height: "500px",
+            overflowY: "scroll",
+            overflowX:"hidden",
+            cursor: "pointer"
+        }} className= {`dropdown-menu ${dropDownClass}`}>
+{results.map((result)=>{
+    return(
+        <li className='dropdown-item' key={result.symbol}>{result.description} ({result.symbol})</li>
+    )
+})}
+        </ul>
+    )
+      }
+    useEffect(()=>{
+        let isMounted = true
+        const fetchData = async ()=>{
+            try {
+                const response = await finnHub.get("/search",{
+                    params: {
+                        q: search
+                    }
+                })
+
+                if (isMounted){
+                    setResults(response.data.result)
+                }
+            } catch (error) {
+console.log(error)
+            }
+        }
+        if(search?.length > 0){
+            fetchData()
+        } else{
+            setResults([])
+        }
+        return ()=>{
+            isMounted = false
+        }
+    }, [search])
   return (
-    <div>AutoComplete</div>
+    <div className='w-50 p-5 rounded mx-auto'>AutoComplete
+<div className="form-floating dropdown">
+<input style={{backgroundColor:"rgba(145,158,171,0.04)"}} id="search"
+type="text" value={search} onChange={(e)=>setSearch(e.target.value)} className='form-control' placeholder='Search' autoComplete='off'/>
+<label htmlFor='search'>Search</label>
+{renderDropdown()}
+</div>
+    </div>
   )
 }
 
